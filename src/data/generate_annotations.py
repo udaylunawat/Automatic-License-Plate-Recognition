448035e1cbd867bb3d42c446be3e9fffdf73f713
udaylunawat <udaylunawat@gmail.com>
2020-08-02 08:47:19 +0530

    For annotations and loading pretrained model

---
+++ src/data/generate_annotations.py
@@ -0,0 +1,23 @@
+from src import config
+
+def pretrained_model_load():
+    urllib.request.urlretrieve(config.URL_MODEL, config.PRETRAINED_MODEL)
+    print('Downloaded pretrained model to ' + config.PRETRAINED_MODEL)
+
+
+def annot_classes_generator():
+
+    config.train_df.to_csv(config.ANNOTATIONS_FILE, index=False, header=None)
+    
+    classes = set(['license_plate'])
+    with open(config.CLASSES_FILE, 'w') as f:
+        for i, line in enumerate(sorted(classes)):
+            f.write('{},{}\n'.format(line,i))
+
+def main():
+    annot_classes_generator()
+    pretrained_model_load()
+
+if __name__ == '__main__':
+    
+    main()
