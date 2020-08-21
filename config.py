793bc150466f6339b226187052de35463ff7f9ce
udaylunawat <udaylunawat@gmail.com>
2020-08-21 17:48:57 +0530

    refactor

--- config.py
+++ config.py
@@ -19,9 +19,23 @@ LABEL_PATH = DIR_PATH + labels
 # INPUT_PATH = DIR_PATH+ input_videos
 # VIDEO_PATH = DIR_PATH + input_videos
 
-LABELS = open(LABEL_PATH).read().strip().split('\n')
 
-COLORS = np.random.randint(0, 255, size = (len(LABELS), 3), dtype = 'uint8')
+# RETINANET CONSTANTS
+##################################
+# load label to names mapping for visualization purposes
+
+labels_to_names = {0: 'number_plate'}
+
+
+
+# YOLO CONSTANTS
+###################################
+
+MIN_CONF = 0.5
+NMS_THRESH = 0.3
 
 DEFAULT_CONFIDENCE = 0.5
 NMS_THRESHOLD = 0.3
+
+LABELS = open(LABEL_PATH).read().strip().split('\n')
+COLORS = np.random.randint(0, 255, size = (len(LABELS), 3), dtype = 'uint8')
