be80aa7c6927dcb2001bc4f6b8a6a18ebb9f6347
udaylunawat <udaylunawat@gmail.com>
2020-08-22 14:24:30 +0530

    update: added various enhancements

--- utils/enhance.py
+++ utils/enhance.py
@@ -2,12 +2,51 @@ import streamlit as st
 import cv2
 import numpy as np
 from PIL import Image,ImageEnhance
+import re
 
 crop, image = None, None
 img_size, crop_size = 600, 400
 
-def cannize_image(our_image):
-    new_img = np.array(our_image.convert('RGB'))
+
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+ 
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+    
+#erosion
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+#skew correction
+def deskew(image):
+  coords = np.column_stack(np.where(image > 0))
+  angle = cv2.minAreaRect(coords)[-1]
+  if angle < -45:
+    angle = -(90 + angle)
+  else:
+      angle = -angle
+  (h, w) = image.shape[:2]
+  center = (w // 2, h // 2)
+  M = cv2.getRotationMatrix2D(center, angle, 1.0)
+  rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+  return rotated
+
+def cannize_image(image):
+    new_img = np.array(image.convert('RGB'))
     img = cv2.cvtColor(new_img,1)
     img = cv2.GaussianBlur(img, (11, 11), 0)
     canny = cv2.Canny(img, 100, 150)
@@ -26,39 +65,64 @@ def cropped_image(image, b):
 def enhance_crop(crop):
 
     st.write("## Enhanced License Plate")
-
+    rgb = np.array(crop.convert('RGB'))
+    gray = cv2.cvtColor(rgb,cv2.COLOR_BGR2GRAY)
     slider = st.sidebar.empty()
-    enhance_type = st.sidebar.radio("Enhance Type",["Original","Gray-Scale","Contrast","Brightness","Blurring","Cannize"])
+    enhance_type = st.sidebar.radio("Enhance Type",\
+                                   ["Original","Gray-Scale","Contrast",\
+                                    "Brightness","Blurring","Cannize",\
+                                    "Remove_noise", "Thresholding", "Dilate",\
+                                    "Opening","Erode", "Deskew", "Custom"])
     
+    rate = slider.slider(enhance_type,0.2,8.0,(1.5))
 
     if enhance_type == 'Original':
         output_image = crop
-        st.image(output_image, width=crop_size, caption = enhance_type)
 
     elif enhance_type == 'Gray-Scale':
-        temp = np.array(crop.convert('RGB'))
-        output_image = cv2.cvtColor(temp,cv2.COLOR_BGR2GRAY)
-        st.image(output_image, width = crop_size, caption = enhance_type)
+        output_image = cv2.cvtColor(rgb,cv2.COLOR_BGR2GRAY)
+        # output_image = get_grayscale(crop)
 
     elif enhance_type == 'Contrast':
-        c_rate = slider.slider("Contrast",0.2,8.0,(3.5))
         enhancer = ImageEnhance.Contrast(crop)
-        output_image = enhancer.enhance(c_rate)
-        st.image(output_image,width = crop_size, caption = enhance_type)
+        output_image = enhancer.enhance(rate)
 
     elif enhance_type == 'Brightness':
-        c_rate = slider.slider("Brightness",0.2,8.0,(1.5))
         enhancer = ImageEnhance.Brightness(crop)
-        output_image = enhancer.enhance(c_rate)
-        st.image(output_image, width = crop_size, caption = enhance_type)
+        output_image = enhancer.enhance(rate)
 
     elif enhance_type == 'Blurring':
-        temp = np.array(crop.convert('RGB'))
-        blur_rate = slider.slider("Blur",0.2,8.0,(1.5))
-        img = cv2.cvtColor(temp,1)
-        output_image = cv2.GaussianBlur(img,(11,11),blur_rate)
-        st.image(output_image, width = crop_size, caption = enhance_type)
+        img = cv2.cvtColor(rgb,1)
+        output_image = cv2.GaussianBlur(img,(11,11),rate)
     
     elif enhance_type == 'Cannize':
         output_image = cannize_image(crop)
-        st.image(output_image, width = crop_size, caption = enhance_type)
+
+    elif enhance_type == "Remove_noise":
+        output_image = remove_noise(rgb)
+
+    elif enhance_type == "Thresholding":
+        output_image = thresholding(gray)
+
+    elif enhance_type == "Dilate":
+        output_image = dilate(rgb)
+
+    elif enhance_type == "Opening":
+        output_image = opening(rgb)
+
+    elif enhance_type == "Erode":
+        output_image = erode(rgb)
+
+    elif enhance_type == "Deskew":
+        output_image = deskew(np.array(gray))
+
+    elif enhance_type == "Custom":
+        # resized = cv2.resize(gray, interpolation=cv2.INTER_CUBIC)
+        dn_gray = cv2.fastNlMeansDenoising(gray, templateWindowSize=7, h=25)
+        gray_bin = cv2.threshold(dn_gray, 80, 255, cv2.THRESH_BINARY)[1]
+        output_image = gray_bin
+
+    st.image(output_image, width = crop_size, caption = enhance_type)
+
+
+
