f176a01712e65179d7bf555fe55b80a13a40aef1
udaylunawat <udaylunawat@gmail.com>
2020-08-25 00:12:21 +0530

    removing download options

--- serve/app.py
+++ serve/app.py
@@ -41,65 +41,6 @@ import webbrowser
 import keras.backend.tensorflow_backend as tb
 tb._SYMBOLIC_SCOPE.value = True
 
-# External files to download.
-EXTERNAL_DEPENDENCIES = {
-    "/content/Automatic-License-Plate-Recognition/output/models/inference/plate_inference_tf2_2.h5": {
-        "url": "https://storage.googleapis.com/dracarys3_bucket/Public/plate_inference_tf2_2.h5",
-        "size": 146111272
-    },
-    "/content/Automatic-License-Plate-Recognition/output/models/inference/yolov3-custom_last.weights": {
-        "url": "https://storage.googleapis.com/dracarys3_bucket/Public/yolov3-custom_last.weights",
-        "size": 246305388
-    }
-}
-
-import urllib
-# This file downloader demonstrates Streamlit animation.
-def download_file(file_path):
-    # Don't download the file twice. (If possible, verify the download using the file length.)
-    if os.path.exists(file_path):
-        print("file name:{}, size:{}".format(file_path, os.path.getsize(file_path)))
-        if "size" not in EXTERNAL_DEPENDENCIES[file_path]:
-            return
-        elif os.path.getsize(file_path) == EXTERNAL_DEPENDENCIES[file_path]["size"]:
-            return
-
-    # These are handles to two visual elements to animate.
-    weights_warning, progress_bar = None, None
-    try:
-        weights_warning = st.warning("Downloading %s..." % file_path)
-        progress_bar = st.progress(0)
-        with open(file_path, "wb") as output_file:
-            with urllib.request.urlopen(EXTERNAL_DEPENDENCIES[file_path]["url"]) as response:
-                length = int(response.info()["Content-Length"])
-                counter = 0.0
-                MEGABYTES = 2.0 ** 20.0
-                while True:
-                    data = response.read(8192)
-                    if not data:
-                        break
-                    counter += len(data)
-                    output_file.write(data)
-
-                    # We perform animation by overwriting the elements.
-                    weights_warning.warning("Downloading %s... (%6.2f/%6.2f MB)" %
-                        (file_path, counter / MEGABYTES, length / MEGABYTES))
-                    progress_bar.progress(min(counter / length, 1.0))
-
-    # Finally, we remove these visual elements by calling .empty().
-    finally:
-        if weights_warning is not None:
-            weights_warning.empty()
-        if progress_bar is not None:
-            progress_bar.empty()
-
-# Download external dependencies.info = st.empty()
-if st.checkbox("MAYBE DOWNLOAD 250MB OF DATA TO THE SERVER. THIS MIGHT TAKE A FEW MINUTES!"):
-
-
-    for filename in EXTERNAL_DEPENDENCIES.keys():
-        print("file name:{}, size:{}".format(filename, os.path.getsize(filename)))
-        download_file(filename)
 #============================ About ==========================
 def about():
 
