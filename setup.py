06fb10762b2f0cb46b409b92ff6527b611f5f0d2
udaylunawat <udaylunawat@gmail.com>
2021-05-01 07:25:02 +0530

    refactor

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='A short description of the project.',
-    author='udaylunawat',
+    author='uday lunawat',
     license='MIT',
 )
